5c655812e94ce1cdde05c7916f65611a6c78a8f5
AlbaBM <alba.bossoms@gmail.com>
2019-12-10 11:02:53 +0100

    Added paths to download dataset

--- analysis.R
+++ analysis.R
@@ -1,3 +1,7 @@
 # Gapminder analysis repository
 
 # first let's download the gapminder file
+
+download.file("https://raw.githubusercontent.com/swcarpentry/r-novice-gapminder/gh-pages/_episodes_rmd/data/gapminder_data.csv", destfile = "data/gapminder_data.csv")
+
+gapminder <- read.csv("data/gapminder_data.csv")
